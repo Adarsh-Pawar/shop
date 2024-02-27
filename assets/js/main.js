fdebf30fb5425178bfaf1678ac683f014b522d69
Adarsh Pawar <pawar.adarsh8178@gmail.com>
2024-02-27 19:06:23 +0530

    adding loaders

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Arsha
-* Updated: Jan 29 2024 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
@@ -161,7 +154,9 @@
   let preloader = select('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
-      preloader.remove()
+      setTimeout(() => {
+        preloader.remove();
+      }, 2000);
     });
   }
 
